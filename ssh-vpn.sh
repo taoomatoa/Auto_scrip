f063b3a2ec537113992f7aa6991e1e90e3f5bf66
Taoo Matoa <131985316+taoomatoa@users.noreply.github.com>
2025-06-24 13:29:55 +0700

    Update ssh-vpn.sh

--- ssh-vpn.sh
+++ ssh-vpn.sh
@@ -207,8 +207,8 @@ sed -i '$ i\echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6' /etc/rc.local >/d
 
 # set time GMT +8
 sleep 1
-echo -e "[ ${green}INFO$NC ] Set zona local time to Asia/Kuala_lumpur GMT+8"
-ln -fs /usr/share/zoneinfo/Asia/Kuala_lumpur /etc/localtime
+echo -e "[ ${green}INFO$NC ] Set zona local time to Asia/Jakarta WIB+8"
+ln -fs /usr/share/zoneinfo/Asia/Jakarta /etc/localtime
 
 # set locale
 sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
